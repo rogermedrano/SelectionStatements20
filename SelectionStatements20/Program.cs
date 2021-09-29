60f26f047e27cf81982a2a7293dece62a0303e2a
Roger Medrano <email@example.com>
2021-09-29 08:09:42 -0700

    first

---
+++ SelectionStatements20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SelectionStatements20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNum = 80;
+            Console.WriteLine("Try guessing my favorite number.....");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNum)
+                Console.WriteLine("Too low!");
+            else if (userInput > favNum)
+                Console.Write("Too high!");
+            else
+                Console.WriteLine("Nevermind.");
+            
+        }
+    }
+}
